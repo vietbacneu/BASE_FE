c44c6540fe3a9e68784e1ce7e66e8e6ac5a41b6e
manhtq1197 <Taquangmanh97>
2021-10-25 02:06:28 +0700

    manhtq commit san pham

---
+++ src/main/webapp/app/modules/qlkh-manager/dl-nhap-hang/nhap-hang/them-sua-nhap-hang/chi-tiet-don-nhap/chi-tiet-don-nhap.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'jhi-chi-tiet-don-nhap',
+  templateUrl: './chi-tiet-don-nhap.component.html',
+  styleUrls: ['./chi-tiet-don-nhap.component.scss']
+})
+export class ChiTietDonNhapComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
