719d5c919454f50b5ad4d7572b6690c601937247
manhtq1197 <Taquangmanh97>
2021-11-11 22:40:41 +0700

    manhtq commit

---
+++ src/main/webapp/app/modules/qlkh-manager/danh-muc/phuong-thuc-thanh-toan/them-sua-phuong-thuc-thanh-toan/them-sua-phuong-thuc-thanh-toan.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'jhi-them-sua-phuong-thuc-thanh-toan',
+  templateUrl: './them-sua-phuong-thuc-thanh-toan.component.html',
+  styleUrls: ['./them-sua-phuong-thuc-thanh-toan.component.scss']
+})
+export class ThemSuaPhuongThucThanhToanComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
