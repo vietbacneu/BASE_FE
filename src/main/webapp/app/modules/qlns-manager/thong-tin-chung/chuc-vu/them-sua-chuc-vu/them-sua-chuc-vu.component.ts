04f06c8fe40197a05e86234e2743004d5fc9a4e3
VB <vietbacneu@gmail.com>
2021-12-04 21:43:10 +0700

    bacnv commit

--- src/main/webapp/app/modules/qlns-manager/thong-tin-chung/chuc-vu/them-sua-chuc-vu/them-sua-chuc-vu.component.ts
+++ src/main/webapp/app/modules/qlns-manager/thong-tin-chung/chuc-vu/them-sua-chuc-vu/them-sua-chuc-vu.component.ts
@@ -12,183 +12,187 @@ import {ToastService} from "app/shared/services/toast.service";
 import {ITEMS_PER_PAGE, MAX_SIZE_PAGE} from "app/shared/constants/pagination.constants";
 
 @Component({
-  selector: 'jhi-them-sua-chuc-vu',
-  templateUrl: './them-sua-chuc-vu.component.html',
-  styleUrls: ['./them-sua-chuc-vu.component.scss']
+    selector: 'jhi-them-sua-chuc-vu',
+    templateUrl: './them-sua-chuc-vu.component.html',
+    styleUrls: ['./them-sua-chuc-vu.component.scss']
 })
 export class ThemSuaChucVuComponent implements OnInit {
 
 
-  @Input() public selectedData;
-  @Input() type;
-  @Output() response = new EventEmitter<any>();
+    @Input() public selectedData;
+    @Input() type;
+    @Output() response = new EventEmitter<any>();
 
 
-  form: FormGroup;
-  height: number;
-  itemsPerPage: any;
-  maxSizePage: any;
-  routeData: any;
-  page: number;
-  totalItems: any;
-  previousPage: any;
-  predicate: any;
-  formValue;
-  reverse: any;
+    form: FormGroup;
+    height: number;
+    itemsPerPage: any;
+    maxSizePage: any;
+    routeData: any;
+    page: number;
+    totalItems: any;
+    previousPage: any;
+    predicate: any;
+    formValue;
+    reverse: any;
 
-  constructor(
-      public translateService: TranslateService,
-      private shareDataFromProjectService: ShareDataFromProjectService,
-      private heightService: HeightService,
-      private activatedRoute: ActivatedRoute,
-      private formBuilder: FormBuilder,
-      private spinner: NgxSpinnerService,
-      protected router: Router,
-      protected commonService: CommonService,
-      public activeModal: NgbActiveModal,
-      private thongTinNhanSuApiService: ThongTinNhanSuApiService,
-      private toastService: ToastService
-  ) {
-    this.itemsPerPage = ITEMS_PER_PAGE;
-    this.maxSizePage = MAX_SIZE_PAGE;
-    this.routeData = this.activatedRoute.data.subscribe(data => {
-      if (data && data.pagingParams) {
-        this.page = data.pagingParams.page;
-        this.previousPage = data.pagingParams.page;
-        this.reverse = data.pagingParams.ascending;
-        this.predicate = data.pagingParams.predicate;
-      }else {
-        this.page = 1;
-      }
-    });
-  }
-
-
-  ngOnInit(): void {
-    this.buildForm()
-  }
-
-
-  buildForm() {
-    this.form = this.formBuilder.group({
-      maChucVu: [null, Validators.required],
-      tenChucVu: [null, Validators.required],
-      mieuTa: [null],
-    });
-    if (this.selectedData) {
-      this.form.patchValue(this.selectedData);
+    constructor(
+        public translateService: TranslateService,
+        private shareDataFromProjectService: ShareDataFromProjectService,
+        private heightService: HeightService,
+        private activatedRoute: ActivatedRoute,
+        private formBuilder: FormBuilder,
+        private spinner: NgxSpinnerService,
+        protected router: Router,
+        protected commonService: CommonService,
+        public activeModal: NgbActiveModal,
+        private thongTinNhanSuApiService: ThongTinNhanSuApiService,
+        private toastService: ToastService
+    ) {
+        this.itemsPerPage = ITEMS_PER_PAGE;
+        this.maxSizePage = MAX_SIZE_PAGE;
+        this.routeData = this.activatedRoute.data.subscribe(data => {
+            if (data && data.pagingParams) {
+                this.page = data.pagingParams.page;
+                this.previousPage = data.pagingParams.page;
+                this.reverse = data.pagingParams.ascending;
+                this.predicate = data.pagingParams.predicate;
+            } else {
+                this.page = 1;
+            }
+        });
     }
-  }
-  changePageSize(size) {
-    this.itemsPerPage = size;
-  }
 
-  trimSpace(element) {
-    const value = this.getValueOfField(element);
-    if (value) {
-      this.setValueToField(element, value.trim());
+
+    ngOnInit(): void {
+        this.buildForm()
     }
-  }
 
-  getValueOfField(item) {
-    return this.form.get(item).value;
-  }
 
-  setValueToField(item, data) {
-    this.form.get(item).setValue(data);
-  }
-
-  onCancel() {
-    this.form.reset();
-    this.activeModal.dismiss(true);
-  }
-
-  sort() {
-    const result = [this.predicate + "," + (this.reverse ? "asc" : "desc")];
-    if (this.predicate !== "sysUserId") {
-      result.push("sysUserId");
+    buildForm() {
+        this.form = this.formBuilder.group({
+            maChucVu: [null, Validators.required],
+            tenChucVu: [null, Validators.required],
+            mieuTa: [null],
+            heSoLuong: [null, Validators.required],
+        });
+        if (this.selectedData) {
+            this.form.patchValue(this.selectedData);
+        }
     }
-    return result;
-  }
 
-  onSubmit() {
-    if (this.form.invalid) {
-      this.commonService.validateAllFormFields(this.form);
-      return;
+    changePageSize(size) {
+        this.itemsPerPage = size;
     }
-    this.spinner.show();
-    const data = {
-      id: null,
-      maChucVu: this.form.value.maChucVu,
-      tenChucVu: this.form.value.tenChucVu,
-      mieuTa: this.form.value.mieuTa,
-    };
-    if (this.type === "add") {
-      this.thongTinNhanSuApiService
-          .createChucVu(data).subscribe(
-          res => {
-            this.spinner.hide();
-            this.toastService.openSuccessToast(
-                "Thêm mới thành công"
+
+    trimSpace(element) {
+        const value = this.getValueOfField(element);
+        if (value) {
+            this.setValueToField(element, value.trim());
+        }
+    }
+
+    getValueOfField(item) {
+        return this.form.get(item).value;
+    }
+
+    setValueToField(item, data) {
+        this.form.get(item).setValue(data);
+    }
+
+    onCancel() {
+        this.form.reset();
+        this.activeModal.dismiss(true);
+    }
+
+    sort() {
+        const result = [this.predicate + "," + (this.reverse ? "asc" : "desc")];
+        if (this.predicate !== "sysUserId") {
+            result.push("sysUserId");
+        }
+        return result;
+    }
+
+    onSubmit() {
+        if (this.form.invalid) {
+            this.commonService.validateAllFormFields(this.form);
+            return;
+        }
+        this.spinner.show();
+        const data = {
+            id: null,
+            maChucVu: this.form.value.maChucVu,
+            tenChucVu: this.form.value.tenChucVu,
+            mieuTa: this.form.value.mieuTa,
+            heSoLuong: this.form.value.heSoLuong,
+        };
+        if (this.type === "add") {
+            this.thongTinNhanSuApiService
+                .createChucVu(data).subscribe(
+                res => {
+                    this.spinner.hide();
+                    this.toastService.openSuccessToast(
+                        "Thêm mới thành công"
+                    );
+                    this.response.emit(true)
+                    this.onCloseModal();
+                },
+                error => {
+                    this.toastService.openErrorToast(
+                        this.translateService.instant("common.toastr.messages.error.load")
+                    );
+                    this.response.emit(false)
+                    this.spinner.hide();
+                }
             );
-            this.response.emit(true)
-            this.onCloseModal();
-          },
-          error => {
-            this.toastService.openErrorToast(
-                this.translateService.instant("common.toastr.messages.error.load")
+        }
+        if (this.type === "update") {
+            if (this.selectedData !== undefined) data.id = this.selectedData.id;
+            this.thongTinNhanSuApiService
+                .updateChucVu(data).subscribe(
+                res => {
+                    this.spinner.hide();
+                    this.toastService.openSuccessToast(
+                        "Cập nhập thành công"
+                    );
+                    this.response.emit(true)
+                    this.onCloseModal();
+                },
+                error => {
+                    this.toastService.openErrorToast(
+                        this.translateService.instant("common.toastr.messages.error.load")
+                    );
+                    this.response.emit(false)
+                    this.spinner.hide();
+                }
             );
-            this.response.emit(false)
-            this.spinner.hide();
-          }
-      );
+        }
     }
-    if (this.type === "update") {
-      if (this.selectedData !== undefined) data.id = this.selectedData.id;
-      this.thongTinNhanSuApiService
-          .updateChucVu(data).subscribe(
-          res => {
-            this.spinner.hide();
-            this.toastService.openSuccessToast(
-                "Cập nhập thành công"
-            );
-            this.response.emit(true)
-            this.onCloseModal();
-          },
-          error => {
-            this.toastService.openErrorToast(
-                this.translateService.instant("common.toastr.messages.error.load")
-            );
-            this.response.emit(false)
-            this.spinner.hide();
-          }
-      );
+
+    onCloseModal() {
+        this.form.reset();
+        this.activeModal.dismiss(true);
     }
-  }
 
-  onCloseModal() {
-    this.form.reset();
-    this.activeModal.dismiss(true);
-  }
+    onResize() {
+        this.height = this.heightService.onResizeWithoutFooter();
+    }
 
-  onResize() {
-    this.height = this.heightService.onResizeWithoutFooter();
-  }
+    formatPreShow(content: string) {
+        if (content.length > 60) return content.substring(0, 60) + "...";
+        else return content;
+    }
 
-  formatPreShow(content: string) {
-    if (content.length > 60) return content.substring(0, 60) + "...";
-    else return content;
-  }
-  isFieldValid(field: string) {
-    return !this.form.get(field).valid && this.form.get(field).touched;
-  }
+    isFieldValid(field: string) {
+        return !this.form.get(field).valid && this.form.get(field).touched;
+    }
 
-  get formControl() {
-    return this.form.controls;
-  }
+    get formControl() {
+        return this.form.controls;
+    }
 
-  numberOnly(event): boolean {
-    const charCode = (event.which) ? event.which : event.keyCode;
-    return !(charCode > 31 && (charCode < 48 || charCode > 57));
-  }
+    numberOnly(event): boolean {
+        const charCode = (event.which) ? event.which : event.keyCode;
+        return !(charCode > 31 && (charCode < 48 || charCode > 57));
+    }
 }
